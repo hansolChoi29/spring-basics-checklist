24d8f9fb2780f47c8d2bd22d38df6190f7987160
hansolChoi29 <ggksthf29@gmail.com>
2025-08-28 13:22:22 +0900

    first commit

---
+++ src/main/java/com/example/springbasicstraining/SpringBasicsTrainingApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbasicstraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicsTrainingApplication  {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World1");
+        SpringApplication.run(SpringBasicsTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbasicstraining/SpringBasicsTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbasicstraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicsTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
